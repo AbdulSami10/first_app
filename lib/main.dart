d34532acfa6c4df9baa5804afc10a4c95494fe79
AbdulSami10 <sami.sayani10@gmail.com>
2023-06-19 16:08:48 +0500

    gradient added

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,15 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.indigo,
         body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 29, 2, 80),
+                Color.fromARGB(255, 49, 2, 80),
+              ],
+            ),
+          ),
           child: const Center(
             child: Text("Hello World"),
           ),
