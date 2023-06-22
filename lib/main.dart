ce3e54e1ca76e97f0e12e1e0eaca10a7d463c9ab
AbdulSami10 <sami.sayani10@gmail.com>
2023-06-22 13:29:22 +0500

    refactor:super key for a class

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,30 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.indigo,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 29, 2, 80),
-                Color.fromARGB(255, 49, 2, 80),
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text("Hello World",
-                style: TextStyle(color: Colors.white, fontSize: 28)),
-          ),
-        ),
-      ),
+    const MaterialApp(
+      home: Scaffold(backgroundColor: Colors.indigo, body: GradientColor()),
     ),
   );
+}
+
+class GradientColor extends StatelessWidget {
+  const GradientColor({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 29, 2, 80),
+            Color.fromARGB(255, 49, 2, 80),
+          ],
+        ),
+      ),
+      child: const Center(
+        child: Text("Hello World",
+            style: TextStyle(color: Colors.white, fontSize: 28)),
+      ),
+    );
+  }
 }
