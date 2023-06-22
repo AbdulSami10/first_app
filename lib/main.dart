a15b280bf1732551bb4a4d31b0523954187561ad
AbdulSami10 <sami.sayani10@gmail.com>
2023-06-22 13:38:25 +0500

    refactor:code splitted into multiple files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:first_app/gradient_color.dart";
 
 void main() {
   runApp(
@@ -6,26 +7,4 @@ void main() {
       home: Scaffold(backgroundColor: Colors.indigo, body: GradientColor()),
     ),
   );
-}
-
-class GradientColor extends StatelessWidget {
-  const GradientColor({super.key});
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 29, 2, 80),
-            Color.fromARGB(255, 49, 2, 80),
-          ],
-        ),
-      ),
-      child: const Center(
-        child: Text("Hello World",
-            style: TextStyle(color: Colors.white, fontSize: 28)),
-      ),
-    );
-  }
 }
