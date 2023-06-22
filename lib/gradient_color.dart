df852a77feb7ea36023d0fee1ba7650410294e08
AbdulSami10 <sami.sayani10@gmail.com>
2023-06-22 14:43:42 +0500

    refactor:dynamic gradient color

--- lib/gradient_color.dart
+++ lib/gradient_color.dart
@@ -5,16 +5,18 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientColor extends StatelessWidget {
-  const GradientColor({super.key});
+  const GradientColor(this.colorOne, this.colorTwo, this.colorThree,
+      {super.key});
+  final Color colorOne, colorTwo, colorThree;
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 29, 2, 80),
-            Color.fromARGB(255, 49, 2, 80),
+            colorOne,
+            colorTwo,
+            colorThree,
           ],
           begin: startAlignment,
           end: endAlignment,
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,13 @@ import "package:first_app/gradient_color.dart";
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(backgroundColor: Colors.indigo, body: GradientColor()),
+      home: Scaffold(
+          backgroundColor: Colors.indigo,
+          body: GradientColor(
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 29, 2, 80),
+            Color.fromARGB(255, 49, 2, 80),
+          )),
     ),
   );
 }
