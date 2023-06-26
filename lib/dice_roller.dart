e54863000be5951cf14256b8f33d537a85dc46cd
AbdulSami10 <sami.sayani10@gmail.com>
2023-06-26 14:19:44 +0500

    random dice app done

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -9,10 +12,10 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = "assets/images/dice-2.png";
+  var currentDiceNumber = 2;
   void rollDice() {
     setState(() {
-      activeDiceImage = "assets/images/dice-4.png";
+      currentDiceNumber = randomizer.nextInt(6) + 1;
     });
   }
 
@@ -22,7 +25,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          "assets/images/dice-$currentDiceNumber.png",
           width: 200,
         ),
         const SizedBox(
