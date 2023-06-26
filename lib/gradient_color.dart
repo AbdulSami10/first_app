0851e3c3922acca6c31081393e29172a75dd5243
AbdulSami10 <sami.sayani10@gmail.com>
2023-06-26 13:56:49 +0500

    stateful widget

--- lib/gradient_color.dart
+++ lib/gradient_color.dart
@@ -1,13 +1,14 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
-void rollDice() {}
 
 class GradientColor extends StatelessWidget {
   const GradientColor(this.colorOne, this.colorTwo, this.colorThree,
       {super.key});
   final Color colorOne, colorTwo, colorThree;
+
   @override
   Widget build(context) {
     return Container(
@@ -22,27 +23,9 @@ class GradientColor extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            "assets/images/dice-2.png",
-            width: 200,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextButton(
-            onPressed: rollDice,
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(fontSize: 28),
-            ),
-            child: const Text("Roll the dice"),
-          )
-        ],
-      )),
+      child: const Center(
+        child: DiceRoller(),
+      ),
     );
   }
 }
